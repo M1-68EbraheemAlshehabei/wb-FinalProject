833e9da5ef741c55f2c2a26cbd47bb1ab3422630
M1-68EbraheemAlshehabei <76258901+M1-68EbraheemAlshehabei@users.noreply.github.com>
2021-01-12 23:12:38 +0300

    second draft

--- scripts.js
+++ scripts.js
@@ -21,3 +21,33 @@ function validation(){
     alert("Form Submitted Successfully")
     return false;
 }
+
+// Function for read more the the kuwait nature page. will allow the user to click the button to read more
+function ReadMoreFunction() {
+
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  // the loading pag procces function
+
+let myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("page").style.display = "block";
+}
